e061178cf76286cd2482191349fcdcc30e1224e3
Chantepierre <lucas@lucassifoni.info>
2024-03-19 09:01:29 +0100

    Documents the diagnostic module

--- lib/dts_buddy/diagnostic.ex
+++ lib/dts_buddy/diagnostic.ex
@@ -3,13 +3,47 @@ defmodule DtsBuddy.Diagnostic do
   Runs an overlay through the compilation and loading cycle, then proceeds to check
   if there really is the expected outcome on the header.
 
-  To run a diagnostic, you
+  To run a diagnostic cycle, you should call run_diagnostic/1, passing a reader function
+  as an argument. This is so that we do not directly depend on Circuits.GPIO as this is
+  not strictly needed to use DtsBuddy.
+
+  Example use, succeeding :
+
+      iex> {:ok, gpio} = Circuits.GPIO.open("GPIO12", :input)
+      {:ok, %Circuits.GPIO.CDev{}}
+      iex> reader = fn () -> Circuits.GPIO.read(gpio)
+      #Function<>
+      iex> DtsBuddy.Diagnostic.run_diagnostic(reader)
+      [DtsBuddy.Diagnostic] Running system checks
+      [DtsBuddy.Diagnostic] All system checks passed
+      [DtsBuddy.Diagnostic] Loading a sample overlay
+      [DtsBuddy.Diagnostic] Sampling GPIO with the user-supplied reader function for ~5 seconds
+      [DtsBuddy.Diagnostic] Behavior of the activated GPIO looks correct with 5 LOW samples and 5 HIGH samples
+      [DtsBuddy.Diagnostic] Unloading overlay testled
+      [DtsBuddy.Diagnostic] Everything looks OK
+      :ok
+
+  Example use, failing :
+
+      iex> {:ok, gpio} = Circuits.GPIO.open("GPIO12", :input)
+      {:ok, %Circuits.GPIO.CDev{}}
+      iex> reader = fn () -> Circuits.GPIO.read(gpio)
+      #Function<>
+      iex> DtsBuddy.Diagnostic.run_diagnostic(reader)
+      [DtsBuddy.Diagnostic] Running system checks
+      [DtsBuddy.Diagnostic] Device tree compiler is missing on the system.
+      [DtsBuddy.Diagnostic] All tests did not pass. Please refer to the above logs for further details.
+      :error
   """
 
-  defp log(msg) do
+  @doc false
+  @spec log(any()) :: :ok
+  def log(msg) do
     IO.puts("[DtsBuddy.Diagnostic] #{msg}")
   end
 
+  @doc false
+  @spec run_diagnostic() :: :ok
   def run_diagnostic do
     IO.puts("""
     DtsBuddy.Diagnostic.run_diagnostic/0 is a placeholder. You should call
@@ -23,19 +57,46 @@ defmodule DtsBuddy.Diagnostic do
     """)
   end
 
+  @doc false
+  @spec run_diagnostic(fun()) :: :error | :ok
   def run_diagnostic(reader_fn) do
     with :ok <- run_system_checks(),
          {:ok, _, name} = compilation_result <- compile_sample(),
          :ok <- DtsBuddy.load(compilation_result),
          samples <- gather_samples(reader_fn),
          :ok <- valid_samples(samples),
-         :ok <- DtsBuddy.unload(name) do
+         :ok <- unload(name) do
+      log("Everything looks OK.")
+      :ok
     else
-      _ -> log("All checks did not pass. Please refer to the above logs for further details.")
+      _ ->
+        log("All tests did not pass. Please refer to the above logs for further details.")
+        :error
     end
   end
 
-  defp valid_samples(samples) do
+  @doc false
+  @spec unload(binary()) :: :error | :ok
+  def unload(name) do
+    log("Unloading overlay #{name}")
+    DtsBuddy.unload(name)
+
+    case DtsBuddy.status(name) do
+      :applied ->
+        log("Unloading overlay #{name} failed")
+        :error
+
+      _ ->
+        :ok
+    end
+  end
+
+  @doc """
+  Checks if the samples collected by the reader function look correct, in the sense that there was enough variation
+  of output in the given time. An erratic GPIO would currently look correct if it was checked by this function though.
+  """
+  @spec valid_samples(any()) :: :error | :ok
+  def valid_samples(samples) do
     keyed = Enum.frequencies(samples)
 
     case keyed do
@@ -52,7 +113,12 @@ defmodule DtsBuddy.Diagnostic do
     end
   end
 
-  defp gather_samples(reader_fn) do
+  @doc """
+  Gathers samples to check the actual behavior of the LED overlay by sampling GPIO roughly every 100ms.
+  Output should be a list of 1 and 0s.
+  """
+  @spec gather_samples(fun()) :: list(integer())
+  def gather_samples(reader_fn) do
     log("Sampling GPIO with the user-supplied reader function for ~5 seconds")
 
     Task.await_many(
@@ -60,11 +126,13 @@ defmodule DtsBuddy.Diagnostic do
         Process.sleep(i * 100)
         reader_fn.()
       end),
-      10000
+      10_000
     )
   end
 
-  defp run_system_checks do
+  @doc false
+  @spec run_system_checks() :: :error | :ok
+  def run_system_checks do
     log("Running system checks")
 
     case DtsBuddy.Backend.checks() do
@@ -82,7 +150,9 @@ defmodule DtsBuddy.Diagnostic do
     end
   end
 
-  defp compile_sample do
+  @doc false
+  @spec compile_sample() :: {:error, {any(), pos_integer()}} | {:ok, binary(), binary()}
+  def compile_sample do
     import DtsBuddy.Sigil
     log("Loading a sample overlay")
 
